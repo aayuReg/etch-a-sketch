f75b4ca87d26eae6956ea26ef510cf744abed3c7
Aayush Regmi <aayushreg10@gmail.com>
2023-06-19 20:22:56 +0545

    Fixed hover by adding custom css

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const container = document.querySelector(".container");
-const userInputBtn=document.querySelector('.userInputBtn');
-let userInput=0;
+const userInputBtn = document.querySelector(".userInputBtn");
+let allDivs = [];
 
+const createDivs = (userinp) => {
+  container.innerHTML = "";
+  container.style.setProperty('--userInput', userinp); //set custom css
+  for (i = 0; i < userinp; i++) {
+    for (j = 0; j < userinp; j++) {
+      let divs = document.createElement("div");
+      divs.classList.add("grid");
 
-
-let allDivs=[];
-    for (let i=0;i<16;i++){
-        for(let j=0;j<16;j++){
-            let divs=document.createElement('div');
-            divs.classList.add('grid')
-            container.appendChild(divs)
-            allDivs.push(divs)
-        }
+      container.appendChild(divs);
+      allDivs.push(divs);
     }
+  }
+  allDivs.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.classList.add("hover");
+    });
+    div.addEventListener("mouseout", () => {
+      div.classList.remove("hover");
+    });
+  });
+};
 
-
-userInputBtn.addEventListener('click',()=>{
-    let userInput=prompt('enter the number of grids you want')
-   
-    
-})
-
-allDivs.forEach(div=>{
-    div.addEventListener('mouseover',()=>{
-        div.classList.add('hover')
-    })
-    // div.addEventListener('mouseout',()=>{
-    //     div.classList.remove('hover')
-    // })
-})
+userInputBtn.addEventListener("click", () => {
+  let userInput = prompt("Enter the size:");
+  userInput = parseInt(userInput);
+  if (userInput < 2 || userInput > 99) {
+    alert("please enter between 2 and 99");
+  } else {
+    createDivs(userInput);
+  }
+});
